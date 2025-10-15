82327fa9eef047c6111e1ee337ac961221f95f16
firatdemir47 <98283697+firatdemir47@users.noreply.github.com>
2025-10-15 09:30:38 +0300

    feat: initialize DreamMapper project structure

---
+++ src/main/java/com/dreammapper/Starter/DreammapperApplicationStarter.java
@@ -0,0 +1,13 @@
+package com.dreammapper.Starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreammapperApplicationStarter {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DreammapperApplicationStarter.class, args);
+	}
+
+}
---
+++ src/test/java/com/dreammapper/DreammapperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dreammapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreammapperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

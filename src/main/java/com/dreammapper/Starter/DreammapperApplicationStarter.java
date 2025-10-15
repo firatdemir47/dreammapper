657d0fe7b96ef66e3093634c3529f066ce4a7b68
firatdemir47 <98283697+firatdemir47@users.noreply.github.com>
2025-10-15 17:08:09 +0300

    fix(Starter): enable full package scanning

--- src/main/java/com/dreammapper/Starter/DreammapperApplicationStarter.java
+++ src/main/java/com/dreammapper/Starter/DreammapperApplicationStarter.java
@@ -3,9 +3,13 @@ package com.dreammapper.Starter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EntityScan(basePackages = {"com.dreammapper"})
+@ComponentScan(basePackages = "com.dreammapper")
+@EnableJpaRepositories(basePackages = "com.dreammapper")
 public class DreammapperApplicationStarter {
 
 	public static void main(String[] args) {

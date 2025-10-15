b90a52358319f61b5b0e4a794e83ea7fc168b7ba
firatdemir47 <98283697+firatdemir47@users.noreply.github.com>
2025-10-15 10:16:28 +0300

    fix(Starter): entity scan config

--- src/main/java/com/dreammapper/Starter/DreammapperApplicationStarter.java
+++ src/main/java/com/dreammapper/Starter/DreammapperApplicationStarter.java
@@ -2,8 +2,10 @@ package com.dreammapper.Starter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = {"com.dreammapper"})
 public class DreammapperApplicationStarter {
 
 	public static void main(String[] args) {

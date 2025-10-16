8bfdd164d6a2ef610f68568cf2714edfd368b93a
firatdemir47 <98283697+firatdemir47@users.noreply.github.com>
2025-10-16 11:58:50 +0300

    refactor(controller): update UserController to use DTOs

--- src/main/java/com/dreammapper/controller/UserController.java
+++ src/main/java/com/dreammapper/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.dreammapper.controller;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -11,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.dreammapper.dto.UserDTO;
+import com.dreammapper.mapper.UserMapper;
 import com.dreammapper.model.User;
 import com.dreammapper.service.UserService;
 
@@ -24,19 +27,22 @@ public class UserController {
 	private final UserService userService;
 
 	@PostMapping
-	public ResponseEntity<User> createUser(@RequestBody User user) {
+	public ResponseEntity<UserDTO> createUser(@RequestBody UserDTO userDTO) {
+		User user = UserMapper.toEntity(userDTO);
 		User savedUser = userService.saveUser(user);
-		return ResponseEntity.ok(savedUser);
+		return ResponseEntity.ok(UserMapper.toDTO(savedUser));
 	}
 
 	@GetMapping
-	public ResponseEntity<List<User>> getAllUsers() {
-		return ResponseEntity.ok(userService.getAllUsers());
+	public ResponseEntity<List<UserDTO>> getAllUsers() {
+		List<UserDTO> users = userService.getAllUsers().stream().map(UserMapper::toDTO).collect(Collectors.toList());
+		return ResponseEntity.ok(users);
 	}
 
 	@GetMapping("/{id}")
-	public ResponseEntity<User> getUserById(@PathVariable Long id) {
-		return userService.getUserById(id).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+	public ResponseEntity<UserDTO> getUserById(@PathVariable Long id) {
+		return userService.getUserById(id).map(UserMapper::toDTO).map(ResponseEntity::ok)
+				.orElse(ResponseEntity.notFound().build());
 	}
 
 	@DeleteMapping("/{id}")

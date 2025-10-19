865a797f5be56ddb547dd51991e717168337d89c
firatdemir47 <98283697+firatdemir47@users.noreply.github.com>
2025-10-19 16:51:09 +0300

    feat(dto): add AnalysisResultDTO

---
+++ src/main/java/com/dreammapper/dto/AnalysisResultDTO.java
@@ -0,0 +1,26 @@
+package com.dreammapper.dto;
+
+import java.util.List;
+import java.util.Map;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AnalysisResultDTO {
+
+	private String summary;
+
+	private String dominantEmotion;
+
+	private List<String> symbols;
+
+	private Map<String, Double> scores;
+}

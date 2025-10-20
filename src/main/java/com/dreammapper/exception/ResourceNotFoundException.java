80a9171f543f09a641ea5468437b14590ba961f0
firatdemir47 <98283697+firatdemir47@users.noreply.github.com>
2025-10-20 12:27:16 +0300

    feat(exception): global handler added

---
+++ src/main/java/com/dreammapper/exception/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.dreammapper.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+	public ResourceNotFoundException(String message) {
+		super(message);
+	}
+}

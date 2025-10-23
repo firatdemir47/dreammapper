a45f4b766767cf18e1ddeea7906ae694a8d0512c
firatdemir47 <98283697+firatdemir47@users.noreply.github.com>
2025-10-23 11:23:47 +0300

    feat(service): add password encoding to UserServiceImpl

--- src/main/java/com/dreammapper/service/impl/UserServiceImpl.java
+++ src/main/java/com/dreammapper/service/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.dreammapper.service.impl;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.dreammapper.model.User;
@@ -15,11 +16,14 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
-	private final UserRepository userRepository;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
 	@Override
 	public User saveUser(User user) {
-
+		if (user.getPassword() != null) {
+			user.setPassword(passwordEncoder.encode(user.getPassword()));
+		}
 		return userRepository.save(user);
 	}
 

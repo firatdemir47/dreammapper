3ddff597a5c75a28531fa571291bcc6b735609e5
firatdemir47 <98283697+firatdemir47@users.noreply.github.com>
2025-11-12 18:22:20 +0300

    fix(config): use SimpleClientHttpRequestFactory instead of HttpComponents

--- src/main/java/com/dreammapper/config/RestTemplateConfig.java
+++ src/main/java/com/dreammapper/config/RestTemplateConfig.java
@@ -5,14 +5,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import java.time.Duration;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 
 @Configuration
 public class RestTemplateConfig {
 	
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
-        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
         factory.setConnectTimeout((int) Duration.ofSeconds(5).toMillis());
         factory.setReadTimeout((int) Duration.ofSeconds(15).toMillis());
         return builder.requestFactory(() -> factory).build();
